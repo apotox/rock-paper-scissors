918f5933f5a34bcb815d8e15ffee1f38c2a36726
saphidev <saphidev@gmail.com>
2022-01-08 15:37:45 +0100

    fix server app port

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const gameInstance = new GameApp(IO);
 app.get('/rooms', (req, res) => res.json(gameInstance.getRooms()))
 
 app.use(express.static('client'));
-
-server.listen(3000, ()=>{
+const {PORT = 3000} = process.env;
+server.listen(PORT, ()=>{
     console.log('listening on port 3000');
 });
 
